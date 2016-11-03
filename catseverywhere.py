52f83f7ef3a737fb4f365c12be1abe0efb36bbba
Bram Pauwelyn <brampauw@gmail.com>
2016-11-03 18:41:14 +0100

    first commit

---
+++ catseverywhere.py
@@ -0,0 +1,40 @@
+from flask import request, redirect
+from flask import Flask, render_template
+from flask import g
+import sqlite3
+from random import randint
+
+app = Flask(__name__)
+
+email_addresses = []
+
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect("emails.db")
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.form['email']
+    g.db.execute("INSERT INTO email_addresses VALUES (?)", [email])
+    g.db.commit()
+    return redirect('/')
+
+@app.route('/emails.html')
+def emails():
+    email_addresses = g.db.execute("SELECT email from email_addresses").fetchall()
+    return render_template('emails.html', email_addresses=email_addresses)
+
+@app.route('/')
+def hello_world():
+    name = "Cats Everywhere"
+    number = randint(0,300)
+    print number
+    return render_template('index.html', name=name, number=number)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ turtle.py
@@ -0,0 +1,11 @@
+names = ("Bram", "Alex")
+list_of_beverages = ("Club Mate", "Coca Cola", "Ice Tea")
+
+for name in names:
+    print("hello "+ name)
+
+for i in range(10):
+    print(i)
+
+for drink in list_of_beverages:
+    print("Would you like a " + drink + "?")
